fc90de2cabca1e1828769c33d27c779123171fca
Vladyslav Yakovlev <yakovlev15@icloud.com>
2021-04-13 04:11:44 +0300

    config database

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -4,15 +4,15 @@ import Vapor
 
 public func configure(_ app: Application) throws {
     
-    app.databases.use(.postgres(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
-    ), as: .psql)
-    
-    app.migrations.add(CreateTodo())
+    if var config = Environment.get("DATABASE_URL")
+        .flatMap(URL.init)
+        .flatMap(PostgresConfiguration.init) {
+        config.tlsConfiguration = .forClient(
+            certificateVerification: .none)
+        app.databases.use(.postgres(
+            configuration: config
+        ), as: .psql)
+    }
     
     try routes(app)
     
